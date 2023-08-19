53d44734e6a7ec13f395c0b1cb779df43a5d1f4a
Dairon <daironpotes73@gmail.com>
2023-08-19 12:04:19 -0500

    Cambio de fondo y agregando funcionalidades

--- main.js
+++ main.js
@@ -1,41 +1,47 @@
-const d = document;
-
-const fecha = d.querySelector("#fecha");
-const lista = d.querySelector("#lista");
-const input = d.querySelector("#input");
-const botonEnter = d.querySelector("#enter");
+const fecha = document.querySelector("#fecha");
+const lista = document.querySelector("#lista");
+const elemento = document.querySelector("#elemento");
+const input = document.querySelector("#input");
+const botonEnter = document.querySelector("#boton-enter");
 const check = "fa-check-circle";
 const uncheck = "fa-circle";
 const lineThrough = "line-through";
-let id;
-let LIST;
+let LIST = [];
 
-//CREACION DE FECHA
+let id = 0;
+
+//creacion de fecha actualizada
+
 const FECHA = new Date();
 fecha.innerHTML = FECHA.toLocaleDateString("es-CO", {
   weekday: "long",
-  month: "short",
+  year: "numeric",
+  month: "long",
   day: "numeric",
 });
 
-//funcion agg tarea
+// funcion de agregar tarea
+
 function agregarTarea(tarea, id, realizado, eliminado) {
   if (eliminado) {
     return;
   }
 
-  const REALIZADO = realizado ? check : uncheck;
+  const REALIZADO = realizado ? check : uncheck; // si realizado es verdadero check si no uncheck
+
   const LINE = realizado ? lineThrough : "";
 
-  const elemento = ` <li id="elemento">
-  <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
-  <p class="text ${LINE}">${tarea}</p>
-  <i class="fas fa-trash de" data="eliminado" id="${id}"></i>
-  `;
+  const elemento = `
+                        <li id="elemento">
+                        <i class="far ${REALIZADO}" data="realizado" id="${id}"></i>
+                        <p class="text ${LINE}">${tarea}</p>
+                        <i class="fas fa-trash de" data="eliminado" id="${id}"></i> 
+                        </li>
+                    `;
   lista.insertAdjacentHTML("beforeend", elemento);
 }
 
-//FUNCION DE TAREA REALIZADA
+// funcion de Tarea Realizada
 
 function tareaRealizada(element) {
   element.classList.toggle(check);
@@ -44,12 +50,12 @@ function tareaRealizada(element) {
   LIST[element.id].realizado = LIST[element.id].realizado ? false : true;
 }
 
-//FUNCION TAREA ELIMINADA
-
 function tareaEliminada(element) {
   element.parentNode.parentNode.removeChild(element.parentNode);
   LIST[element.id].eliminado = true;
 }
+
+// crear un evento para escuchar el enter y para habilitar el boton
 
 botonEnter.addEventListener("click", () => {
   const tarea = input.value;
@@ -61,14 +67,13 @@ botonEnter.addEventListener("click", () => {
       realizado: false,
       eliminado: false,
     });
+    input.value = "";
+    id++;
   }
-  localStorage.setItem("TODO", JSON.stringify(LIST));
-  input.value = "";
-  id++;
 });
 
-d.addEventListener("keyup", function (e) {
-  if (e.key == "Enter") {
+document.addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
     const tarea = input.value;
     if (tarea) {
       agregarTarea(tarea, id, false, false);
@@ -78,38 +83,18 @@ d.addEventListener("keyup", function (e) {
         realizado: false,
         eliminado: false,
       });
+      input.value = "";
+      id++;
     }
-    localStorage.setItem("TODO", JSON.stringify(LIST));
-    input.value = "";
-    id++;
   }
 });
 
-lista.addEventListener("click", function (e) {
-  const element = e.target;
+lista.addEventListener("click", function (event) {
+  const element = event.target;
   const elementData = element.attributes.data.value;
   if (elementData === "realizado") {
     tareaRealizada(element);
   } else if (elementData === "eliminado") {
     tareaEliminada(element);
   }
-  localStorage.setItem("TODO", JSON.stringify(LIST));
 });
-
-//LOCAL STORAGE GET ITEM
-
-let data = localStorage.getItem("TODO");
-if (data) {
-  LIST = JSON.parse(data);
-  id = LIST.length;
-  cargarLista(LIST);
-} else {
-  LIST = [];
-  id = 0;
-}
-
-function cargarLista(DATA) {
-  DATA.forEach(function (i) {
-    agregarTarea(i.nombre, i.id, i.realizado, i.eliminado);
-  });
-}
